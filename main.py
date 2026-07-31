88ea59382ca6f30ccd7189a99bd3342f2c3b8e26
Mzoli <167242849+mzolisomi@users.noreply.github.com>
2026-08-01 01:45:09 +0200

    update: Stage 1: root and health endpoints

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 async def HelloWorld():
     return { "message" : "Hello, world" }
 
+
+@app.get('/health')
+async def HealthStatus():
+    return { "name" : "Task API", "version" : "1.0" , "endpoints" : ["/tasks"] }
+
