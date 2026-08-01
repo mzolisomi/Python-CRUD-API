9d4d90e30f690b695bc8b37aa5505cc5cfb0af54
Mzoli <167242849+mzolisomi@users.noreply.github.com>
2026-08-01 20:21:18 +0200

    update: Stage 4: full CRUD

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 class TaskCreate(BaseModel):
     title : str
+
+class TaskUpdate(BaseModel):
+    title : Optional[str] = None
+    done : Optional[bool] = None
 
 tasks = [
     {
@@ -66,3 +71,42 @@ async def AddTask(req: TaskCreate):
 
     tasks.append(task)
     return task
+
+@app.put("/tasks/{task_id}")
+async def UpdateTask(task_id: int, req: TaskUpdate):
+    if req.title is None and req.done is None:
+        return JSONResponse(
+            status_code = 400,
+            content= { "error" : "Please update title or status of the task"}
+        )
+
+    if req.title is not None and req.title.strip() == "":
+        return JSONResponse(
+            status_code = 400,
+            content={ "error" : "Please provide a title"}
+        )
+
+    for task in tasks:
+        if task["id"] == task_id:
+            if req.title is not None:
+                task["title"] = req.title
+            if req.done is not None:
+                task["done"] = req.done
+            return task
+    return JSONResponse(
+        status_code = 404,
+        content = { "error" : f"No task with id '{task_id}'" }
+    )
+
+        
+
+
+@app.delete("/tasks/{task_id}")
+async def DeleteTask(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            return Response(status_code = 204)
+    return Response(
+            status_code = 404
+        )
